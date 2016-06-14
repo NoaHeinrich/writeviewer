4fae4fe6f9e2998934117b226d8b343c10320987
Noah Heinrich <noahheinrich@gmail.com>
2016-06-14 10:55:11 -0500

    Add User model tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe User do
+  before(:each) do
+    @user = User.new(username: "test", email: "test@test.com", password: "password")
+  end
+
+  let(:user) { User.new }
+  describe "#new" do
+    it "creates a valid user object" do
+      expect(@user.valid?).to be true
+    end
+
+    it "recognizes an invalid email" do
+      user = User.new(username: "test", email: "test", password: "password")
+      expect(user.valid?).to be false
+    end
+  end
+end
