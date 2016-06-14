3e5ca793cb7ec733e3115eca7822cb902632db99
Noah Heinrich <noahheinrich@gmail.com>
2016-06-14 11:02:29 -0500

    Add Video class method

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,4 +9,15 @@ class Video < ActiveRecord::Base
   validates :thumbnail, presence: true
 
   pg_search_scope :search_by_title, :against => :title
+
+  def get_video_info
+    video_info = VideoInfo.new(self.url)
+    unless video_info.available?
+      raise RuntimeError, "The url you entered was not valid. Please ry again"
+    else
+      self.embed_code = video_info.embed_code
+      self.title = video_info.title
+      self.thumbnail = video_info.thumbnail_medium
+    end
+  end
 end
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Video do
+  before(:each) do
+    @video = Video.new()
+  end
+end
